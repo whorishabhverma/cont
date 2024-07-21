f0a9ec6ff59609ada338acb8728c0c2bc53219f8
rishabh verma <2021aspire28@gmail.com>
2024-07-21 21:29:44 +0530

    topo added

---
+++ dsa/topological_sort.cpp
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+	//Function to return list containing vertices in Topological order.
+	void dfs(vector<int> adj[],int u,vector<bool> &visited,stack<int> &st){
+	    visited[u]=true;
+	    for(int &v:adj[u]){
+	        if(!visited[v]){
+	            dfs(adj,v,visited,st);
+	        }
+	    }
+	    st.push(u);
+	}
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    stack<int> st;
+	    vector<bool> visited(V,0);
+	    for(int i=0;i<V;i++){
+	        if(!visited[i]){
+	            dfs(adj,0,visited,st);
+	        }
+	    }
+	    vector<int> ans;
+	    while(!st.empty()){
+	        ans.push_back(st.top());
+	        st.pop();
+	    }
+	    return ans;
+	    //stack se vector
+	    
+	}
+};
+
+//{ Driver Code Starts.
+
+/*  Function to check if elements returned by user
+*   contains the elements in topological sorted form
+*   V: number of vertices
+*   *res: array containing elements in topological sorted form
+*   adj[]: graph input
+*/
+int check(int V, vector <int> &res, vector<int> adj[]) {
+    
+    if(V!=res.size())
+    return 0;
+    
+    vector<int> map(V, -1);
+    for (int i = 0; i < V; i++) {
+        map[res[i]] = i;
+    }
+    for (int i = 0; i < V; i++) {
+        for (int v : adj[i]) {
+            if (map[i] > map[v]) return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N, E;
+        cin >> E >> N;
+        int u, v;
+
+        vector<int> adj[N];
+
+        for (int i = 0; i < E; i++) {
+            cin >> u >> v;
+            adj[u].push_back(v);
+        }
+        
+        Solution obj;
+        vector <int> res = obj.topoSort(N, adj);
+
+        cout << check(N, res, adj) << endl;
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
