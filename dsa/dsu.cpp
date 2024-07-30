fafc5f39905a6d40f8fffe420ea5ed8f6b346b4d
rishabh verma <2021aspire28@gmail.com>
2024-07-30 23:10:18 +0530

    addded segment tree notes

--- dsa/dsu.cpp
+++ dsa/dsu.cpp
@@ -18,28 +18,43 @@ video : 2
 
 
 // path compression
-
-int find(int i,vector<int> &parent){
-    if(i==parent[i]) return i;
-    return parent[i] = find(parent[i],parent);
-}
-//by rank
-void union(int x,int y,vector<int>&parent,vector<int>&rank){
-    int x_parent = find(x,parent);
-    int y_parent = find(y,parent);
-    if(x_parent == y_parent) return;
-    if(rank[x_parent] > rank[y_parent]){
-        parent[y_parent] = x_parent;
+class DSU {
+public:
+    vector<int> parent;
+    vector<int> rank;
+    
+    //parent.resize();
+    //rank.resize(V,0);
+    
+    DSU(int size) {
+        parent.resize(size);
+        rank.resize(size, 0);
+        for (int i = 0; i < size; i++) {
+            parent[i] = i;
+        }
     }
-    else if(rank[x_parent] < rank[y_parent]){
-        parent[X_parent] = y_parent;
-
+    
+    int find(int i) {
+        if (i == parent[i]) return i;
+        return parent[i] = find(parent[i]);
     }
-    else{
-        //kisi ko parent bnao and rank bdhao
-        parent[X_parent] = y_parent;
-        rank[y_parent]+=1;
+    
+    void Union(int x, int y) {
+        int x_parent = find(x);
+        int y_parent = find(y);
+        if (x_parent == y_parent) return;
+        
+        if (rank[x_parent] > rank[y_parent]) {
+            parent[y_parent] = x_parent;
+        }
+        else if (rank[x_parent] < rank[y_parent]) {
+            parent[x_parent] = y_parent;
+        }
+        else {
+            parent[x_parent] = y_parent;
+            rank[y_parent]++;
+        }
     }
-}
+};
 
 */
